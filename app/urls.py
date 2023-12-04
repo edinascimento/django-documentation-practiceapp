55ad22365ffc0a48617244b7c40d0c5f306a4388
Edipo Nascimento <esnedipo@gmail.com>
2023-12-04 11:34:46 -0300

    added more detailed view

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "app"
 urlpatterns = [
+    # ex: /app/
     path("", views.index, name="index"),
+    # ex: /app/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /app/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /app/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
 
+from . models import Question
+
 def index(request):
-    return HttpResponse("Hello, world! You're at the app index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    return render(request, "app/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "app/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
