3a75882bab599dea14c977669639ecec076d5674
Edipo Nascimento <esnedipo@gmail.com>
2023-12-04 15:00:38 -0300

    added generic view

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = "app"
 urlpatterns = [
-    # ex: /app/
-    path("", views.index, name="index"),
-    # ex: /app/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /app/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /app/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
